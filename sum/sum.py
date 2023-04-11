ed96af9cdef9ef56fb1acdd27c6bd4c9a0451ad8
Thirumal Reddy Palwai <thirumalreddy.palwai@philips.com>
2023-04-12 00:43:21 +0530

    initial version

---
+++ sum/sum.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/<num1>/<num2>")
+
+def default(num1,num2):
+    return str(float(num1)+float(num2))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
